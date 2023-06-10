d33fda928e11addbbe29a7d76ae5c1f4f0062df8
himalg7 <thapahimal777@gmail.com>
2023-06-10 12:09:12 +0545

    base url for server

--- core/signals.py
+++ core/signals.py
@@ -9,7 +9,10 @@ import requests
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 from django.core.files import File
-API_BASE_URL = 'http://127.0.0.1:5000'
+
+from core.views.constants import API_BASE_URL
+
+
 @receiver(post_save, sender=Item)
 def clothing_preprocessing(sender, instance, created, **kwargs):
 
